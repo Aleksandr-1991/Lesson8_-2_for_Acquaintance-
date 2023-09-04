71e636e5883f99ab2d5912ec362b69834d634e1f
Aleks37 <lao35180972643335589007>
2023-09-04 20:54:48 +0400

    Переименование папок

---
+++ Lesson8_Example009_IntroArray/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max (int Arg1, int Arg2, int Arg3)
+{
+    int result = Arg1;
+    if (Arg2 > result) result = Arg2;
+    if (Arg3 > result) result = Arg3;
+    return result;
+}
+int [] array = {58,13,11,7,68,72,36,76,32};
+
+// int max1 = Max (a1, b1, c1);
+// int max2 = Max (a2, b2, c2);
+// int max3 = Max (a3, b3, c3);
+// int max = Max (max1, max2, max3);
+// int max = Max (Max (a1, b1, c1), Max (a2, b2, c2), Max (a3, b3, c3));
+int max = Max 
+(
+Max (array[0], array[1], array[2]), 
+Max (array[3], array[4], array[5]), 
+Max (array[6], array[7], array[8])
+);
+Console.WriteLine(max);
---
+++ Lesson8_Example010/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {11, 2, 13, 71, 14, 51, 16, 71, 81};
+int n = array.Length;
+int find = 71;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
